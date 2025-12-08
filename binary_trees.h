2e983d4a3da879913922b7664f9e615b38a32047
LucasN <lucas.n230896@gmail.com>
2025-12-08 11:01:05 +0000

    fix: adding binarytreeprint function

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,10 @@
 #ifndef BINARY_TREES
-#define BINARY_TREES 
+#define BINARY_TREES
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -22,6 +27,7 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
+void binary_tree_print(const binary_tree_t *tree);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 #endif
