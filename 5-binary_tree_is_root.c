3ef38470bdd5d3c1572f65db9a0c7ff2b82f1341
Allix <thewitchonxanax@gmail.com>
2025-12-09 10:08:47 +0000

    fix: remove trailing white spaces for betty coding style

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -4,12 +4,6 @@
  * binary_tree_is_root - checks if a node is a root
  * @node: pointer to the node to check
  * Return: 1 if node is a root, 0 otherwise
- * 
- * 
- * Prototype: int binary_tree_is_root(const binary_tree_t *node);
-Where node is a pointer to the node to check
-Your function must return 1 if node is a root, otherwise 0
-If node is NULL, return 0
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
